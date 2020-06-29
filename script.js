dabe2644f0a5c814a8f88d974e9cde0fd795d336
heatherzen <heather.zen@gmail.com>
2020-06-28 20:44:20 -0700

    initial html and js variables setup

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 var body = document.body;
 
-var h1El = document.createElement('h1');
-h1El.textContent = 'Coding Quiz Challenge';
-h1El.setAttribute('style', 'text-align: center; width: 40%; margin: auto;');
-body.appendChild(h1El);
+// variables for style
+var myTitle = document.getElementById("title").style.textAlign = "center";
+var myIntro = document.getElementById("intro").style.textAlign = "center";
+var startQuiz = document.getElementById("startquiz-container").style.textAlign = "center";
+var myQuestion = document.getElementById("question").style.textAlign = "center";
+var myAnswers = document.getElementById("answer-container").style = ("text-align: center");
+var timerStyle = document.getElementById("timer").style = ("display: flex; justify-content: flex-end; padding: 40px;");
 
-var h4El = document.createElement('h4');
-h4El.textContent = 'Answer the following questions within the time allotted.  Incorrect answers will deduct from the time given.';
-h4El.setAttribute('style', 'font-size: 16px; text-align: center; width: 40%; margin: auto;');
-body.appendChild(h4El);
 
-var timerDisplay = document.getElementsByClassName("timer");
-timerDisplay.setAttribute('style', 'width:10%; display:flex; justify-content:flex-end; align-items:flex-start; margin:auto;');
+// variables for functions
+var startQuizBtn = document.querySelector("#startquiz");
+var quesTions = document.querySelector("#question");
+var timerCountDown = document.querySelector("#countdown");
+var count = 75;
+
+
+
+// startQuizBtn.addEventListener("click",
